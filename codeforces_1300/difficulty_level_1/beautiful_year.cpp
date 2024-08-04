8b4dfebdebaf3de6790c5162edc4c831f72285b9
VikasGoudar <avikashasappeared@gmail.com>
2024-08-04 15:27:32 +0530

    solved more questions

---
+++ codeforces_1300/difficulty_level_1/beautiful_year.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+	int year;
+	std::cin >> year;
+
+	int a,b,c,d;
+
+	while (true){
+		year++;
+		a = year/1000 % 10;
+		b = year/100 % 10;
+		c = year/10 % 10;
+		d = year%10;
+
+		if (a == b || a == c || a == d || b == c || b == d || c == d){
+			continue;			
+		}
+		else{
+			break;
+		}
+	}
+
+	std::cout << year;
+
+	return 0;
+}
---
+++ codeforces_1300/difficulty_level_1/lights_out.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+    int arr[3][3];
+    int res[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cin >> arr[i][j];
+            res[i][j] = 1; // All lights start as on
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (arr[i][j] % 2 == 1) { 
+                res[i][j] ^= 1; 
+
+                if (i - 1 >= 0) res[i - 1][j] ^= 1; 
+                if (i + 1 < 3) res[i + 1][j] ^= 1;  
+                if (j - 1 >= 0) res[i][j - 1] ^= 1;
+                if (j + 1 < 3) res[i][j + 1] ^= 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << res[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
